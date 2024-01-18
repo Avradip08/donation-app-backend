c3247b48bf0117459306f76c8b1f3db5a4de5894
Avradip Das <avrasecret@gmail.com>
2024-01-18 16:54:34 +0530

    initial commit

---
+++ stripe.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const stripe = require("stripe")(process.env.STRIPE_PVT_SECRET);
+
+app.use(express.json());
+app.use(cors());
+
+
+
+//public key api
+app.get("/api/config",async(req,res)=>{
+    // console.log("hello")
+    res.json({pkey :  process.env.STRIPE_PUBLIC_SECRET})
+ 
+})
+
+// checkout api
+app.post("/api/create-checkout-session",async(req,res)=>{
+    const lineItems = [{
+        quantity:1,
+        price_data:{
+            product_data:{
+                name:"donation",
+                
+            },
+            currency: req.body.currency,
+            unit_amount: parseInt(req.body.amount)*100,
+        }
+    }];
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types:["card"],
+        line_items:lineItems,
+        mode:"payment",
+        success_url:"http://localhost:5174",
+        cancel_url:"http://localhost:5174",
+    });
+    res.json({id:session.id})
+})
+
+
+app.listen(7000,()=>{
+    console.log("server start")
+})
